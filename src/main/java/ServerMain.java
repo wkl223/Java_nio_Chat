af8147d1634c6be5a9f83e612c1919343b91f842
Kaile Wang <s3543953@student.rmit.edu.au>
2021-09-15 17:10:35 +1000

    Applied thread pool for selector.

--- src/main/java/ServerMain.java
+++ src/main/java/ServerMain.java
@@ -12,19 +12,19 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Stream;
 
 public class ServerMain {
-  private static final ExecutorService THREAD_POOL = Executors.newFixedThreadPool(10);
+  private ThreadPool pool = new ThreadPool(10);
   private static final String host = "127.0.0.1";
-  private static List<Room> chatRoom;
+  private volatile static List<Room> chatRoom;
   private static ConcurrentHashMap clients; //<key=clientName, value=current room>
-  private static List<String> freeName = new ArrayList<String>();//
+  private volatile static List<String> freeName = new ArrayList<String>();//
   public static final String DEFAULT_ROOM = "MainHall";
   private Room defaultRoom = new Room(DEFAULT_ROOM,"0","");
 
@@ -119,7 +119,6 @@ public class ServerMain {
         }
         // if there is an event.
         Set<SelectionKey> selectionKeys = selector.selectedKeys();
-        //TODO: since selector is a blocking service. We need to implement a thread pool for this!
         for(SelectionKey s: selectionKeys){
           //System.out.println("DEBUG: new event: "+s.toString());
           selectionKeyHandler(s,selector);
@@ -167,7 +166,11 @@ public class ServerMain {
           String message = String.valueOf(StandardCharsets.UTF_8.decode(b));
           String client = (String)selectionKey.attachment();
           System.out.println("DEBUG: received client message: " + message+" from client:"+client);
-          processMessageAndRespond(message,client,selector,selectionKey);
+          WorkerThread worker;
+          do{
+            worker = pool.getWorker();
+          }while(worker == null);
+          worker.processMessageAndRespond(message,client,selector,selectionKey);
           b.clear();
         } catch (IOException e) {
           //something wrong with the client. treat it as quit
@@ -175,127 +178,186 @@ public class ServerMain {
           m.setType(Message.TYPE_QUIT);
           m.setSuccessed(true);
           Protocol p = new Protocol(m);
-          processMessageAndRespond(p.encodeJson(), (String) selectionKey.attachment(),selector,selectionKey);
+          try {
+            WorkerThread worker;
+            do{
+              worker = pool.getWorker();
+            }while(worker == null);
+            worker.processMessageAndRespond(p.encodeJson(), (String) selectionKey.attachment(),selector,selectionKey);
+          } catch (IOException ex) {
+          }
         }catch(Exception e){};//just in case
       }
     }
   }
-  public void processMessageAndRespond(String message, String client, Selector selector,SelectionKey selectionKey) throws IOException {
-    //TODO: broadcast or single response logic per each message type.
-    Protocol p = new Protocol(message);
-    Protocol answer = null;
-    List<String> affectedClients = null;
-    if(p.decodeJson().getType()==null) return;
-    switch (p.decodeJson().getType()){
+  class ThreadPool {
+    List idle = new LinkedList();
+    ThreadPool(int poolSize) {
+      for (int i = 0; i < poolSize; i++) {
+        WorkerThread thread = new WorkerThread(this);
+        thread.setName("Worker" + (i + 1));
+        thread.start();
+        idle.add(thread);
+      }
+    }
+    WorkerThread getWorker() {
+      WorkerThread worker = null;
+      synchronized (idle) {
+        if (idle.size() > 0) {
+          worker = (WorkerThread) idle.remove(0);
+        }
+      }
+      return (worker);
+    }
+    void returnWorker(WorkerThread worker) {
+      synchronized (idle) {
+        idle.add(worker);
+      }
+    }
+  }
+
+  class WorkerThread extends Thread {
+    private ByteBuffer buffer = ByteBuffer.allocate(1024);
+    private ThreadPool pool;
+    private SelectionKey selectionKey;
+    private Selector selector;
+
+    WorkerThread(ThreadPool pool) {
+      this.pool = pool;
+    }
+    public synchronized void run() {
+      System.out.println(this.getName() + " is ready");
+
+      while (true) {
+        try {
+          this.wait();
+        } catch (InterruptedException e) {
+          this.interrupted();
+        }
+        if (selectionKey == null) {
+          continue; // just in case
+        }
+        System.out.println(this.getName() + " has been awakened");
+        this.pool.returnWorker(this);
+      }
+    }
+    public synchronized void processMessageAndRespond(String message, String client, Selector selector,SelectionKey selectionKey) throws IOException {
+      Protocol p = new Protocol(message);
+      Protocol answer = null;
+      List<String> affectedClients = null;
+      if(p.decodeJson().getType()==null) return;
+      switch (p.decodeJson().getType()){
         case Message.TYPE_DELETE: {
           Room defaultRoom = ServerResponds.findRoom(chatRoom, DEFAULT_ROOM);
           Room affectedRoom =ServerResponds.findRoom(chatRoom,p.getMessage().getRoomId());
           answer = ServerReception.deleteRoom(p, client, clients, chatRoom);
           if(affectedRoom != null) {
-              affectedClients = affectedRoom.getUsers();
+            affectedClients = affectedRoom.getUsers();
+          }
+          if (answer.getMessage().isSuccessed()){
+            for(String c: affectedClients){
+              Protocol temp = ServerResponds.roomChange(chatRoom,c,affectedRoom.getRoomId(),DEFAULT_ROOM);
+              //as they are changing rooms, note that it is not a formal room change message
+              //so that no follow-up message in this case. e.g., ROOM CONTENTS and ROOM LIST.
+              singleResponse(temp,c,selector);
+              broadCast(temp, (ArrayList<String>) defaultRoom.users,selector,selectionKey);
+              defaultRoom.users.add(c);
+              clients.remove(c);
+              clients.put(c,DEFAULT_ROOM);
             }
-            if (answer.getMessage().isSuccessed()){
-                for(String c: affectedClients){
-                    Protocol temp = ServerResponds.roomChange(chatRoom,c,affectedRoom.getRoomId(),DEFAULT_ROOM);
-                    //as they are changing rooms, note that it is not a formal room change message
-                    //so that no follow-up message in this case. e.g., ROOM CONTENTS and ROOM LIST.
-                    singleResponse(temp,c,selector);
-                    broadCast(temp, (ArrayList<String>) defaultRoom.users,selector,selectionKey);
-                    defaultRoom.users.add(c);
-                    clients.remove(c);
-                    clients.put(c,DEFAULT_ROOM);
-                }
-            }
-            singleResponse(answer,client,selector);
-            break;
-        }
-        case Message.TYPE_JOIN: {
-            Room formerRoom = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
-            answer = ServerReception.joinRoom(p, client, clients, chatRoom);
-            if (answer.getMessage().getRoomId().equals(DEFAULT_ROOM) && !answer.getMessage().getFormer().equals(DEFAULT_ROOM)) {
-                singleResponse(ServerResponds.roomContents(DEFAULT_ROOM, chatRoom), client, selector);
-                singleResponse(ServerResponds.roomList(chatRoom), client, selector);
-            }
-            if(answer.getMessage().isSuccessed()){
-              Room targetRoom = ServerResponds.findRoom(chatRoom,p.getMessage().getRoomId());
-              List<String> affectedUser;
-              if((targetRoom.users !=null && formerRoom.users!=null)||(targetRoom.users.size()!=0&&formerRoom.users.size()!=0)){
-                affectedUser = new ArrayList<>(formerRoom.users);
-                affectedUser.addAll(targetRoom.users);
-              }
-              else{
-                affectedUser = (targetRoom.users == null)||(targetRoom.users.size()==0)? formerRoom.users:targetRoom.users;
-              }
-              broadCast(answer, (ArrayList<String>) affectedUser,selector,selectionKey);
-              //finally, delete room if the room has no owner and no users.
-              if(formerRoom.users.size()==0 && formerRoom.owner.equals(Message.EMPTY)) chatRoom.remove(formerRoom);
-            }
+          }
           singleResponse(answer,client,selector);
           break;
         }
-      case Message.TYPE_IDENTITY_CHANGE: {
-        Room affected = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
-        answer = ServerReception.identityChange(p, client, clients, freeName, chatRoom);
-        String name = client;
-        if(answer.getMessage().isSuccessed()){
-          broadCast(answer, (ArrayList<String>) affected.users,selector,selectionKey);
-          getKey(selector,answer.getMessage().getFormer()).attach(answer.getMessage().getIdentity());
-          name = answer.getMessage().getIdentity();
-        }
-        singleResponse(answer,name,selector);
-        break;
-      }
-      case Message.TYPE_WHO: {
-        answer = ServerReception.who(p, client, chatRoom);
-        if(answer.getMessage().isSuccessed()) singleResponse(answer,client,selector);
-        break;
-      }
-      case Message.TYPE_QUIT: {
-        //1. remove from current room
-        Room affected = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
-        answer = ServerReception.quit(p, client, chatRoom, clients);
-        affected.users.remove(client);
-        //2. remove all occupied rooms
-        for(Room r: chatRoom){
-          if(r.owner.equals(client)){
-            r.owner="";
+        case Message.TYPE_JOIN: {
+          Room formerRoom = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
+          answer = ServerReception.joinRoom(p, client, clients, chatRoom);
+          if (answer.getMessage().getRoomId().equals(DEFAULT_ROOM) && !answer.getMessage().getFormer().equals(DEFAULT_ROOM)) {
+            singleResponse(ServerResponds.roomContents(DEFAULT_ROOM, chatRoom), client, selector);
+            singleResponse(ServerResponds.roomList(chatRoom), client, selector);
           }
+          if(answer.getMessage().isSuccessed()){
+            Room targetRoom = ServerResponds.findRoom(chatRoom,p.getMessage().getRoomId());
+            List<String> affectedUser;
+            if((targetRoom.users !=null && formerRoom.users!=null)||(targetRoom.users.size()!=0&&formerRoom.users.size()!=0)){
+              affectedUser = new ArrayList<>(formerRoom.users);
+              affectedUser.addAll(targetRoom.users);
+            }
+            else{
+              affectedUser = (targetRoom.users == null)||(targetRoom.users.size()==0)? formerRoom.users:targetRoom.users;
+            }
+            broadCast(answer, (ArrayList<String>) affectedUser,selector,selectionKey);
+            //finally, delete room if the room has no owner and no users.
+            if(formerRoom.users.size()==0 && formerRoom.owner.equals(Message.EMPTY)) chatRoom.remove(formerRoom);
+          }
+          singleResponse(answer,client,selector);
+          break;
         }
-        clients.remove(client);
-        broadCast(answer, (ArrayList<String>) affected.users,selector,selectionKey);
-        singleResponse(answer,client,selector);
-        int port = ((SocketChannel) selectionKey.channel()).socket().getPort();
-        System.out.println("DEBUG - port: "+port+" has disconnected");
-        selectionKey.cancel();
-        selector.wakeup();
-        if(client.matches("Guest[0-9]+")) freeName.add(client);
-        break;
-      }
-      case Message.TYPE_LIST: {
-        answer = ServerReception.list(p, client, chatRoom);
-        singleResponse(answer,client,selector);
-        break;
-      }
-      case Message.TYPE_ROOM_CREATION: {
-        answer = ServerReception.createRoom(p, client, chatRoom);
-        if(answer.getMessage().isSuccessed()) {
-          System.out.println("room created: "+p.getMessage().getRoomId());
+        case Message.TYPE_IDENTITY_CHANGE: {
+          Room affected = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
+          answer = ServerReception.identityChange(p, client, clients, freeName, chatRoom);
+          String name = client;
+          if(answer.getMessage().isSuccessed()){
+            broadCast(answer, (ArrayList<String>) affected.users,selector,selectionKey);
+            getKey(selector,answer.getMessage().getFormer()).attach(answer.getMessage().getIdentity());
+            name = answer.getMessage().getIdentity();
+          }
+          singleResponse(answer,name,selector);
+          break;
         }
-        singleResponse(answer,client,selector);
-        break;
+        case Message.TYPE_WHO: {
+          answer = ServerReception.who(p, client, chatRoom);
+          if(answer.getMessage().isSuccessed()) singleResponse(answer,client,selector);
+          break;
+        }
+        case Message.TYPE_QUIT: {
+          //1. remove from current room
+          Room affected = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
+          answer = ServerReception.quit(p, client, chatRoom, clients);
+          affected.users.remove(client);
+          //2. remove all occupied rooms
+          for(Room r: chatRoom){
+            if(r.owner.equals(client)){
+              r.owner="";
+            }
+          }
+          clients.remove(client);
+          broadCast(answer, (ArrayList<String>) affected.users,selector,selectionKey);
+          singleResponse(answer,client,selector);
+          int port = ((SocketChannel) selectionKey.channel()).socket().getPort();
+          System.out.println("DEBUG - port: "+port+" has disconnected");
+          selectionKey.cancel();
+          selector.wakeup();
+          if(client.matches("Guest[0-9]+")) freeName.add(client);
+          break;
+        }
+        case Message.TYPE_LIST: {
+          answer = ServerReception.list(p, client, chatRoom);
+          singleResponse(answer,client,selector);
+          break;
+        }
+        case Message.TYPE_ROOM_CREATION: {
+          answer = ServerReception.createRoom(p, client, chatRoom);
+          if(answer.getMessage().isSuccessed()) {
+            System.out.println("room created: "+p.getMessage().getRoomId());
+          }
+          singleResponse(answer,client,selector);
+          break;
+        }
+        case Message.TYPE_MESSAGE: {
+          Room affected = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
+          System.out.println(client);
+          System.out.println((String) clients.get(client));
+          answer = ServerReception.message(p, client);
+          broadCast(answer, (ArrayList<String>) affected.users,selector,selectionKey);
+          break;
+        }
+        default:
+          System.out.println("DEBUG - Something is wrong in reception service");
+          break;
       }
-      case Message.TYPE_MESSAGE: {
-        Room affected = ServerResponds.findRoom(chatRoom, (String) clients.get(client));
-        System.out.println(client);
-        System.out.println((String) clients.get(client));
-        answer = ServerReception.message(p, client);
-        broadCast(answer, (ArrayList<String>) affected.users,selector,selectionKey);
-        break;
-      }
-      default:
-        System.out.println("DEBUG - Something is wrong in reception service");
-        break;
     }
+
   }
 
 }
